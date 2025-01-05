bb7278228a61020ae8df9dab2e5c376d5abe9912
lucascorreaa <lucas.lca14@gmail.com>
2025-01-05 00:30:59 -0300

    feat: added async in store using zustand

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -3,11 +3,17 @@ import { create } from "zustand";
 type CounterStore = {
   count: number
   increment: VoidFunction,
+  incrementAsync: () => Promise<void>,
   decrement: VoidFunction
 }
 
 export const useCounterStore = create<CounterStore>((set) => ({
   count: 0,
   increment: () => set((state) => ({count: state.count + 1})),
+  incrementAsync: async () => {
+    // poderíamos adicionar um response de API aqui
+    await new Promise((resolved) => setTimeout(resolved, 1000))
+    set((state) => ({count: state.count + 1}))
+  },
   decrement: () => set((state) => ({count: state.count - 1}))
 }))
