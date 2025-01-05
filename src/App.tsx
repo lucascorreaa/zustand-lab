6f47506be54be4389466a8d9bf78dfd863e3a929
lucascorreaa <lucas.lca14@gmail.com>
2025-01-05 08:12:26 -0300

    feat: adding reset using destructuring

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { useEffect } from "react";
 import { useCounterStore } from "./stores/store";
 
 const setState = () => {
+
   // set um estado
   useCounterStore.setState({ count: 11 })
 }
 
 const logCount = () => {
+
   // busca o estado
   const count = useCounterStore.getState().count
   console.log('count', count)
@@ -20,14 +22,21 @@ const App = () => {
 };
 
 const OtherComponent = ({ count }: { count: number }) => {
+
   // const increment = useCounterStore((state) => state.increment)
   const decrement = useCounterStore((state) => state.decrement)
+
   // esse incremento aguarda 1s para realizar a ação
   const incrementAsync = useCounterStore((state) => state.incrementAsync)
 
+  // buscando as funções usando desestruturação, acredito que afeta a performance por buscar todos as variáveis durante a busca...
+  const { reset } = useCounterStore()
+
   useEffect(() => {
+
     // setState()
     // logCount, para usar esse, precisa adicionar o count como dependência
+
   }, [])
 
   return (
@@ -37,6 +46,7 @@ const OtherComponent = ({ count }: { count: number }) => {
         <button onClick={incrementAsync}>increment</button>
         <button onClick={decrement}>decrement</button>
       </div>
+      <button onClick={reset}>reset</button>
     </div>
   );
 };
