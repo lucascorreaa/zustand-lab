6bdd2e222514ec464e49d62e55897974f7ac3cab
lucascorreaa <lucas.lca14@gmail.com>
2025-01-05 00:05:14 -0300

    feat: added increment and decrement for state

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,8 +2,12 @@ import { create } from "zustand";
 
 type CounterStore = {
   count: number
+  increment: VoidFunction,
+  decrement: VoidFunction
 }
 
-export const useCounterStore = create<CounterStore>(() => ({
-  count: 0
+export const useCounterStore = create<CounterStore>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({count: state.count + 1})),
+  decrement: () => set((state) => ({count: state.count - 1}))
 }))
